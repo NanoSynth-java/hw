ca3478e8adad23dc916934cb04cb6c2d4d3b6188
NanoSynth-java <79605200+NanoSynth-java@users.noreply.github.com>
2021-02-28 17:59:53 +0200

    Final

--- src/zadanie1/Main.java
+++ src/zadanie1/Main.java
@@ -3,6 +3,13 @@ package zadanie1;
 public class Main {
 
     public static void main(String[] args) {
-        // write your code hereg
+        int a = 4;
+        int b = 3;
+        double r = (Math.sqrt(a*a+b*b)/(2)); // r = 2.5
+
+        if (r>2.4){
+                System.out.println("Картонка закрывает прямоугольник");}
+        else if (r<2.5){
+                System.out.println("Картонка маловата");}
+        }
     }
-}
